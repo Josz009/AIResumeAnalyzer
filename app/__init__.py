fc08ee07b5ea751d2b848b5fbe4302bf159e0738
Josz009 <108372810+Josz009@users.noreply.github.com>
2024-12-01 21:06:34 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Flask configurations
 app.config["SECRET_KEY"] = "2ba00670e71b1938dca8df9b7ac91c28c694941a56d4bef8"
-app.config["MONGO_URI"] = "mongodb+srv://josestrada87:Carlos09@healthandwelness.fxsekx1.mongodb.net/AIResumeBuilder?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://jos_estrada87:Carlos09@healthandwelness.fxsekx1.mongodb.net/AIResumeBuilder?retryWrites=true&w=majority"
 
 # Enable CORS
 CORS(app, origins=["https://airesumeanalyzerfrontend.onrender.com"])
