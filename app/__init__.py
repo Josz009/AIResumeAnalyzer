d06c30ca6011b3cd4226d3e8603bc2e8f00f7273
Josz009 <108372810+Josz009@users.noreply.github.com>
2024-12-02 12:12:03 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ app.config["SECRET_KEY"] = "2ba00670e71b1938dca8df9b7ac91c28c694941a56d4bef8"
 app.config["MONGO_URI"] = "mongodb+srv://jos_estrada87:Carlos09@healthandwelness.fxsekx1.mongodb.net/AIResumeBuilder?retryWrites=true&w=majority"
 
 # Enable CORS
-CORS(app, origins=["https://airesumeanalyzerfrontend.onrender.com"])
+CORS(app, origins=[""https://airesumeanalyzer.onrender.com"])
 
 # Initialize MongoDB connection
 mongo = PyMongo(app)
